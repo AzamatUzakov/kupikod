e850345d6cf4e8679336f4c9bf513d7d95cea3a9
AzamatUzakov <azamatakmal053@gmail.com>
2024-04-14 23:33:15 +0500

    aaa

--- mobile game/js/contact-widget-config.js
+++
@@ -1,1 +0,0 @@
-window.hdeContactWidget = { "size": 30, "position": "bottom-right", "direction": "top", "bg": "1E66F8", "color": "000601", "buttons": [{ "source": "telegram", "url": "https:\/\/t.me\/kupikod_bot", "bg": "" }, { "source": "chat", "url": "", "bg": "12536C" }] };
